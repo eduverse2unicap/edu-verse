5b5795239a8827fc145c1f1d5341a37fc336f669
rossini135 <toquehuk@gmai.com>
2025-10-12 18:35:02 -0300

    instituition adding

--- backend/api/banco.py
+++ backend/api/banco.py
@@ -12,8 +12,9 @@ def create_conn():
     except Error as e:
         print(f"Error: {e}")
     return conn
-
-def create_table(conn):
+#tables creation!
+#students
+def create_table_students(conn):
     try:
         cursor = conn.cursor()
         cursor.execute("""
@@ -21,23 +22,50 @@ def create_table(conn):
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 nome TEXT NOT NULL,
                 idade INTEGER NOT NULL,
+                email TEXT UNIQUE,
                 senha TEXT NOT NULL,
+                phone_number TEXT DEFAULT '+xx(xxx)xxxxx-xxxx',
                 level INTEGER NOT NULL DEFAULT 1,
                 xp INTEGER NOT NULL DEFAULT 0,
-                materias TEXT DEFAULT '[]'
+                materias TEXT DEFAULT '[]',
+                cpf TEXT UNIQUE,
+                instituicao TEXT DEFAULT 'None',
+                photo TEXT DEFAULT 'None'
             )
         """)
         conn.commit()
-        print("Table created successfully")
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        if conn:
+            conn.close()
+#instituitions
+def create_table_institutions(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS instituicoes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                email TEXT UNIQUE,
+                senha TEXT NOT NULL,
+                phone_number TEXT DEFAULT '+xx(xxx)xxxxx-xxxx',
+                photo TEXT DEFAULT 'None',
+                descricao TEXT DEFAULT 'None',
+                cursos TEXT DEFAULT '[]',
+                alunos TEXT DEFAULT '[]',
+                professores TEXT DEFAULT '[]'
+            )
+        """)
+        conn.commit()
     except Error as e:
         print(f"Error: {e}")
     finally:
         if conn:
             conn.close()
 
-
 def get_students():
-    create_table(create_conn())
+    create_table_students(create_conn())
     conn = create_conn()
     students = []
     try:
@@ -49,9 +77,15 @@ def get_students():
                 "id": row[0],
                 "nome": row[1],
                 "idade": row[2],
-                "level": row[4],
-                "xp": row[5],
-                "materias": row[6]
+                "email": row[3],
+                "senha": row[4],
+                "phone_number": row[5],
+                "level": row[6],
+                "xp": row[7],
+                "materias": row[8],
+                "cpf": row[9],
+                "instituicao": row[10],
+                "photo": row[9]
             })
     except Error as e:
         print(f"Error: {e}")
@@ -60,19 +94,62 @@ def get_students():
             conn.close()
     return students
 
-def add_student(name, age, password, level=1, xp=0, materias='[]'):
-    create_table(create_conn())
+def add_student(name, age, email, password, level=1, xp=0, materias='[]', cpf=None, instituicao=None, photo=None):
+    create_table_students(create_conn())
     conn = create_conn()
     try:
         cursor = conn.cursor()
         cursor.execute("""
-            INSERT INTO alunos (nome, idade, senha, level, xp, materias)
-            VALUES (?, ?, ?, ?, ?, ?)
-        """, (name, age, password, level, xp, materias))
+            INSERT INTO alunos (nome, idade, email, senha, level, xp, materias, cpf, instituicao, photo)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, (name, age, email, password, level, xp, materias, cpf, instituicao, photo))
         conn.commit()
     except Error as e:
         print(f"Error: {e}")
     finally:
         if conn:
             conn.close()
-        
+
+def get_institutions():
+    create_table_institutions(create_conn())
+    conn = create_conn()
+    institutions = []
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM instituicoes")
+        rows = cursor.fetchall()
+        for row in rows:
+            institutions.append({
+                "id": row[0],
+                "nome": row[1],
+                "email": row[2],
+                "senha": row[3],
+                "phone_number": row[4],
+                "photo": row[5],
+                "descricao": row[6],
+                "cursos": row[7],
+                "alunos": row[8],
+                "professores": row[9]
+            })
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        if conn:
+            conn.close()
+    return institutions
+
+def add_institution(name, email, password, phone_number=None, photo=None, descricao=None, cursos='[]', alunos='[]', professores='[]'):
+    create_table_institutions(create_conn())
+    conn = create_conn()
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO instituicoes (nome, email, senha, phone_number, photo, descricao, cursos, alunos, professores)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, (name, email, password, phone_number, photo, descricao, cursos, alunos, professores))
+        conn.commit()
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        if conn:
+            conn.close()
--- backend/api/main.py
+++ backend/api/main.py
@@ -15,18 +15,58 @@ def get_students():
 class Student(BaseModel):
     name: str
     idade: int
+    email: str = None
     senha: str
+    phone_number: str = None
     level: int = 1
     xp: int = 0
     materias: str = '[]'
+    cpf: str
+    instituicao: str = None
+    photo: str = None
+
+class Institution(BaseModel):
+    name: str
+    address: str = None
+    email: str = None
+    senha: str = None
+    phone_number: str = None
+    photo: str = None
+    description: str = None
+    cursos: str = '[]'
+    alunos: str = '[]'
+    professores: str = '[]'
 
 @app.post("/new-student")
 def create_student(student: Student):
     banco.add_student(
         name = student.name,
         age = student.idade,
+        email = student.email,
         password = student.senha,
+        phone_number = student.phone_number,
         level = student.level,
         xp = student.xp,
-        materias = student.materias
+        materias = student.materias,
+        cpf = student.cpf,
+        instituicao = student.instituicao,
+        photo = student.photo
+    )
+
+@app.get("/institutions")
+def get_institutions():
+    return banco.get_institutions()
+
+@app.post("/new-institution")
+def create_institution(institution: Institution):
+    banco.add_institution(
+        name = institution.name,
+        email = institution.email,
+        password = institution.senha,
+        phone_number = institution.phone_number,
+        photo = institution.photo,
+        descricao = institution.description,
+        cursos = institution.cursos,
+        alunos = institution.alunos,
+        professores = institution.professores
     )
